07777cbb60a5f75ad223dfd50586606a87ad8049
arnb-smnta <loveumearnab.2812000@gmail.com>
2026-09-01 13:23:35 +0530

    fix: pooling issues

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -1,8 +1,12 @@
 import 'dotenv/config'
+
+import dns from 'node:dns'
 import { drizzle, type NodePgDatabase } from 'drizzle-orm/node-postgres'
 import { Pool } from 'pg'
 
 const isProduction = process.env.NODE_ENV === 'production'
+
+dns.setDefaultResultOrder('ipv6first')
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
@@ -15,19 +19,13 @@ const pool = new Pool({
 
     max: 5,
     min: 0,
-
     connectionTimeoutMillis: 10000,
     idleTimeoutMillis: 30000,
-
     keepAlive: true,
     keepAliveInitialDelayMillis: 10000,
-
     maxLifetimeSeconds: 300,
 })
 
 export const db: NodePgDatabase = drizzle(pool, {
     logger: !isProduction,
 })
-
-// vmMF7wOH1ALi8FUd
-// learner-enthusiast's Project
